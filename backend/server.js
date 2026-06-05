a2c176b57624e32fe56a9d3931ab5e089d1632a6
mayank raj <dishuyadav8791@gmail.com>
2026-06-06 04:37:27 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://mongo:27017/mydb';
+mongoose.connect(MONGO_URI)
+    .then(()=> console.log("MongoDB connected"))
+    .catch(err=> console.log(err));
+
+app.get("/api",(req,res)=>{
+    res.json({
+        message: "Node backend running",
+        database: "MongoDB connected"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
